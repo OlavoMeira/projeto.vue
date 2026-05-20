b053e57439bf4269f357290690573b7fa52d37c0
olavomeiradeveloper@gmail.com <olavomeiradeveloper@gmail.com>
2026-05-20 16:03:49 -0300

    palavras reservadas

---
+++ Curso-Udemy/Modulo003-programador-Frontend/8-Operadores-logicos/app1.js
@@ -0,0 +1,30 @@
+// Operadores Lógicos
+
+// and lógico (&&)
+// or lógico (||)
+// not lógico (!)
+
+console.log("&&");
+
+console.log("true && true = ", true && true );
+console.log("true && false = ", true && false );
+console.log("false && true = ", false && true );
+console.log("false && false = ", false && false );
+
+console.log("=================================================",);
+
+console.log("||");
+
+console.log("true || true = ", true || true );
+console.log("true || false = ", true || false );
+console.log("false || true = ", false || true );
+console.log("false || false = ", false || false );
+
+console.log("=================================================",);
+
+console.log("!");
+
+console.log("true ! true = ", !true );
+console.log("true ! false = ", !true  );
+console.log("false ! true = ", !false  );
+console.log("false ! false = ", !false );
